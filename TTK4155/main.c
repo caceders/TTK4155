fd08e33a828d559ad0cb659002b82cc876a1a232
Carl Anders J. Cederström <andersjohnsgaard@hotmail.com>
2024-09-02 12:39:40 +0200

    first commit

---
+++ TTK4155/main.c
@@ -0,0 +1,18 @@
+/*
+ * TTK4155.c
+ *
+ * Created: 02.09.2024 12:14:52
+ * Author : Carl Anders J. Cederström, Simen Haug & Siver Grimstad
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
