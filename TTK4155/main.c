d35a2eccf995bf993ed28051c2fe651bedf379db
Carl Anders J. Cederström <andersjohnsgaard@hotmail.com>
2024-09-05 15:26:59 +0200

    Bound the printf function to the driver

--- TTK4155/main.c
+++ TTK4155/main.c
@@ -6,7 +6,7 @@
  */ 
 #define F_CPU 4915200
 #include <avr/io.h>
-#include "uart.h"
+#include <util/delay.h>
 
 #define FOSC 1843200// Clock Speed
 #define BAUD 9600
@@ -14,11 +14,25 @@
 
 int main(void)
 {
+	// Create a square wave for testing purposes
+	//DDRA = 00000001; // Set PA0 output
+    //while (1) 
+    //{
+	//	PORTA |= (1<<PA0);			// Set PA0 high
+	//	_delay_ms(100);
+	//	PORTA &= ~(1<<PA0);			// Set PA0 low
+	//	_delay_ms(100);
+    //}
+	
 	// Test to echo back received char
-	USART_init(MYUBRR);
-    while (1) 
-    {
-		char test_char = USART_Receive();
-		USART_Transmit(test_char);
-    }
+	//USART_init(MYUBRR);
+	//while (1)
+	//{
+	//	char test_char = USART_Receive();
+	//	USART_Transmit(test_char);
+	//}
+	
+	// Test to bind the printf function
+	fdevopen(&USART_Transmit, &USART_Receive)
+	printf("Hello world!")
 }
