7f15ded5363bf920d0ae15d858cae57adbacfd98
Simen Haug <simehaug@stud.ntnu.no>
2024-09-05 14:43:32 +0200

    Fixed project setup

--- TTK4155/main.c
+++ TTK4155/main.c
@@ -4,21 +4,20 @@
  * Created: 02.09.2024 12:14:52
  * Author : Carl Anders J. Cederström, Simen Haug & Siver Grimstad
  */ 
-#define F_CPU 16000000
+#define F_CPU 4915200
 #include <avr/io.h>
-#include <delay.h>
+#include <util/delay.h>
 
 int main(void)
 {
 	// Create a square wave for testing purposes
-	DDRA = 0000 0001; // Set PA0 output
+	DDRA = 00000001; // Set PA0 output
 	
     while (1) 
     {
 		PORTA |= (1<<PA0);			// Set PA0 high
-		delay_ms(300);
-		PORTA |= (0<<PA0);			// Set PA0 low
-		delay_ms(300);
+		_delay_ms(100);
+		PORTA &= ~(1<<PA0);			// Set PA0 low
+		_delay_ms(100);
     }
 }
-
