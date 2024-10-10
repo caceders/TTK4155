398866839aa97b5f0cd6a269b5d59ced71746eff
Simen Haug <simhau19@gmail.com>
2024-10-10 15:59:01 +0200

    Started work on node2 and fixed some errors in makefile

--- node2/node2/main.c
+++ node2/node2/main.c
@@ -7,15 +7,30 @@
 
 
 #include "sam.h"
+#include "uart.h"
 
+#define F_CPU 84000000
+
+int my_putc(char data, FILE* f )
+{
+	(void)f;
+	uart_tx(data);
+	//textbox_put_char(&tbh, data);
+}
+
+int my_getc(FILE* f){
+    NULL;
+}
 
 int main(void)
 {
+    uart_init(F_CPU, 9600);
+    fdevopen(&my_putc, &my_getc); //redirect stdout/stdin to uart
     /* Initialize the SAM system */
     SystemInit();
 
+    printf("Hello world!");
+
     /* Replace with your application code */
-    while (1) 
-    {
-    }
+    while (1);
 }
