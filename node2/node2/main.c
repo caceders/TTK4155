7898bc414edcfe261ddc69518179dabab2c21b75
Simen Haug <simehaug@stud.ntnu.no>
2024-10-10 11:44:38 +0200

    Added code template from BB

---
+++ node2/node2/main.c
@@ -0,0 +1,21 @@
+/*
+ * node2.c
+ *
+ * Created: 10.10.2024 11:30:08
+ * Author : Simen
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
